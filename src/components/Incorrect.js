dd98f06f2f48e772a09b3f6698686f9c0800dcec
Munnazzah Aslam <aslammunnazzah@gmail.com>
2020-10-06 13:14:54 +0500

    Create Incorrect.js

---
+++ src/components/Incorrect.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Fail() {
+    return (
+        <div>
+            <h2 className="fail">Sorry! You failed!</h2>
+        </div>
+    )
+}
+
+export default Fail;
