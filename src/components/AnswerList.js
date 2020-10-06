eea5ede6a5afbed4d54e42cb4b7d81fb609928c5
Munnazzah Aslam <aslammunnazzah@gmail.com>
2020-10-06 12:46:19 +0500

    Update AnswerList.js

--- src/components/AnswerList.js
+++ src/components/AnswerList.js
@@ -4,6 +4,7 @@ import Answer from './Answer.js';
 function AnswerList(props) {
     const options=[];
     for(let i=0; i < props.dataSet.options.length; i++){
+    //To push options on to the web
         options.push(<Answer choice={i} handleClick={props.handleClick} answer={props.dataSet.options[i]}/>);
     }
     return (
