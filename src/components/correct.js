22fd0be7f2ea951079b4823900808846a79917a3
Munnazzah Aslam <aslammunnazzah@gmail.com>
2020-10-06 13:09:47 +0500

    Create correct.js

---
+++ src/components/correct.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Pass() {
+    return (
+        <div>
+            <h2 className="pass">Congratulations! You passed!</h2>
+        </div>
+    )
+}
+
+export default Pass;
